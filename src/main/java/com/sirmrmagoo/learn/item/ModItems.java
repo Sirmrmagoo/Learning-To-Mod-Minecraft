8f4c6541e97e26acf2aa0ac5d2e15b7cd6580518
Sirmrmagoo <Sirmrmagoo@gmail.com>
2023-03-08 15:25:25 -0800

    0.0.1-1.19.3 Added A Test Weapon And Black Opal

---
+++ src/main/java/com/sirmrmagoo/learn/item/ModItems.java
@@ -0,0 +1,30 @@
+package com.sirmrmagoo.learn.item;
+
+import com.sirmrmagoo.learn.Learn;
+import net.minecraft.commands.arguments.item.ItemInput;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Learn.MODID);
+
+
+    public static final RegistryObject<Item> BLACK_OPAL = ITEMS.register("black_opal",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TEST = ITEMS.register("test",
+            () -> new SwordItem(Tiers.TEST, 5, 15f, new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+    public static class  Tiers {
+        public static final Tier TEST = new ForgeTier(2, 150, 50f, 5, 500,null, () -> Ingredient.of(ItemStack.EMPTY));
+    }
+}
